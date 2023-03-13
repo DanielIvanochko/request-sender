3c12beb4561843246cad3aabb1f2037cbc6015cb
DanielIvanochko <daniel.ivanochko.pz.2020@lpnu.ua>
2023-03-13 11:23:06 +0200

    Initialize project

---
+++ request-sender/src/test/java/request/sender/RequestSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package request.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
