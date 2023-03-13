92202fc20d9ff3c90c00dbf69c6f27f3197ad673
DanielIvanochko <daniel.ivanochko.pz.2020@lpnu.ua>
2023-03-13 13:47:49 +0200

    Create domain Comment class for testing

---
+++ request-sender/src/main/java/request/sender/domain/Comment.java
@@ -0,0 +1,16 @@
+package request.sender.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Comment {
+    private int postId;
+    private int id;
+    private String name;
+    private String email;
+    private String body;
+}
